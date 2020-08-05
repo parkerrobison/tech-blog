014ab6687b62274993875239cf939fcedd666bae
probison <parker.robison@hotmail.com>
2020-08-04 22:53:53 -0600

    added edit and delete post functionality.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../../utils/auth');
 const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
@@ -62,11 +63,11 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_text: req.body.post_text,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -75,7 +76,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -99,7 +100,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -13,7 +13,7 @@ async function signupFormHandler(event) {
             headers: { 'Content-Type': 'application/json' }
         })
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
